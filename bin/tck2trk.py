7ec3e48a4d65861d65dd172f5c2894c6c1350d11
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2017-02-12 22:43:38 -0500

    Updated streamlines conversion scripts

--- bin/tck2trk.py
+++ bin/tck2trk.py
@@ -27,15 +27,17 @@ def main():
     for tractogram in args.tractograms:
         if nib.streamlines.detect_format(tractogram) is not nib.streamlines.TckFile:
             print("Skipping non TCK file: '{}'".format(tractogram))
+            continue
 
         output_filename = tractogram[:-4] + '.trk'
         if os.path.isfile(output_filename) and not args.force:
             print("Skipping existing file: '{}'. Use -f to overwrite.".format(output_filename))
+            continue
 
         header = {}
         header[Field.VOXEL_TO_RASMM] = nii.affine.copy()
-        header[Field.VOXEL_SIZES] = nii.header.get_zooms()
-        header[Field.DIMENSIONS] = nii.shape
+        header[Field.VOXEL_SIZES] = nii.header.get_zooms()[:3]
+        header[Field.DIMENSIONS] = nii.shape[:3]
         header[Field.VOXEL_ORDER] = "".join(aff2axcodes(nii.affine))
 
         tck = nib.streamlines.load(tractogram)
